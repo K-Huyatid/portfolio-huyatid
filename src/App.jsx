fc1911f3c1bac75c9ca1fa6bc495ef0ded1139c0
K-Huyatid <k.huyatid.512896@umindanao.edu.ph>
2024-08-28 14:46:28 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  const getRandomPosition = () => {
+    const randomTop = Math.floor(Math.random() * 80); // random value between 0% and 80%
+    const randomLeft = Math.floor(Math.random() * 80); // random value between 0% and 80%
+    return { top: `${randomTop}%`, left: `${randomLeft}%` };
+  };
+
+  const handleMouseOver = () => {
+    setPosition(getRandomPosition());
+  };
+
+  return (
+    <div
+      className="text-3xl font-bold underline bg-red-300"
+      style={{ position: 'absolute', top: position.top, left: position.left }}
+      onMouseOver={handleMouseOver}
+    >
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
